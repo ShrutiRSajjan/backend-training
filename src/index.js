85e2c6a80a3ebf1dffb51b35bd056e489c457914
shruti <shruti19sajjan@gmail.com>
2022-08-29 00:06:45 +0530

    jj

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://shruti:vl0WOko40b158xl1@cluster0.tlcpiya.mongodb.net/test", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
@@ -20,6 +20,24 @@ app.use (
         next();
   }
   );
+
+  const assignmentMW= function (req, res, next) {
+    var currentdate = new Date(); 
+    var datetime =  currentdate.getDate() + " "
+                    + (currentdate.getMonth()+1)  + " " 
+                    + currentdate.getFullYear() + "  "  
+                    + currentdate.getHours() + ":"  
+                    + currentdate.getMinutes() + ":" 
+                    + currentdate.getSeconds();
+ 
+    let ip= req.ip
+    let url= req.originalUrl
+    console.log(`${datetime}  ${ip}  ${url}`)
+    next()    
+}
+ 
+app.use( assignmentMW )
+
 
 app.use('/', route);
 
